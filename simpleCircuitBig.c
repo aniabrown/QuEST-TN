91a3f85b12be172c8db2ded51adba15fbce3db6d
aniabrown <ania.brown@gmail.com>
2019-05-15 12:41:30 +0100

    fixed bugs in circuit4 example, which includes uncontracted indices for both tensor1 and tensor2.
    Also adding DEBUG macro to switch off print statements.
    
    Contraction output index was not being calculated correctly in this case due to bugs in getContractionVqIndices and the fact that the ordering of virtual qubits wasn't being considered.
    
    VqVertex lists were not being updated correctly -- the link between tensor1 vertices and tensor2 vertices was not established correctly.

---
+++ simpleCircuitBig.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include "QuEST.h"
+#include "QuEST_debug.h"
+#include "tn.h"
+
+int main (int narg, char *varg[]) {
+
+    /*
+     * PREPARE QuEST environment
+     * (Required only once per program)
+     */
+
+    QuESTEnv env = createQuESTEnv();
+
+    printf("-------------------------------------------------------\n");
+    printf("Running QuEST tutorial:\n\t Basic circuit involving a system of 3 qubits.\n");
+    printf("-------------------------------------------------------\n");
+
+    /*
+     * PREPARE QUBIT SYSTEM
+     */
+
+    Qureg qubits = createQureg(4, env);
+    //initStateDebug(qubits);
+    initZeroState(qubits);
+    hadamard(qubits, 1);
+    //hadamard(qubits, 2);
+    reportStateToScreen(qubits, env, 0);
+
+    /*
+     * REPORT SYSTEM AND ENVIRONMENT
+     */
+    printf("\nThis is our environment:\n");
+    reportQuregParams(qubits);
+    reportQuESTEnv(env);
+
+    /*
+     * APPLY CIRCUIT
+     */
+
+    controlledNot(qubits, 1, 2);
+    reportStateToScreen(qubits, env, 0);
+
+
+    /*
+     * TENSOR NETWORK
+     */
+    int numPqPerTensor[2] = {2, 2};
+    int numVqPerTensor[2] = {1, 1};
+    printf("\n\nCreate Tensor Network:\n");
+    TensorNetwork tn = createTensorNetwork(2, numPqPerTensor, numVqPerTensor, env);
+    printTensorNetwork(tn);
+    reportStateToScreen(tn.tensors[0].qureg, env, 0);
+    reportStateToScreen(tn.tensors[1].qureg, env, 0);
+
+    printf("\n\nApply controlled not:\n");
+    tn_controlledNot(tn, 1, 2);
+    printTensorNetwork(tn);
+    reportStateToScreen(tn.tensors[0].qureg, env, 0);
+    reportStateToScreen(tn.tensors[1].qureg, env, 0);
+
+    printf("\n\nContract tensors:\n");
+    contractTensors(tn, 0, 1, env);
+    reportStateToScreen(tn.tensors[0].qureg, env, 0);
+    reportStateToScreen(tn.tensors[1].qureg, env, 0);
+
+
+
+    /*
+     * FREE MEMORY
+     */
+
+    destroyQureg(qubits, env); 
+
+
+    /*
+     * CLOSE QUEST ENVIRONMET
+     * (Required once at end of program)
+     */
+    destroyQuESTEnv(env);
+    return 0;
+}
