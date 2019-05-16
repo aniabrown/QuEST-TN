c146834b9eda5e3875495f85637d1eae4126d37c
aniabrown <ania.brown@gmail.com>
2019-05-16 11:55:28 +0100

    updated naming conventions in TN/tn.h to match those in TN/tn.c

--- TN/tn.h
+++ TN/tn.h
@@ -11,12 +11,18 @@
     These are not capitalised by default to distinguish ordinary vars from structs such as
     VqVertex, which do have the first letter capitalised. 
 
-    Unless marked as global in the name, all qubit indices refer to local index within 
-    a tensor and qubit type. i.e. physical qubits in tensor 2 start from index 0, 
+    Unless marked as global in the name, all var names that refer to qubit indices refer 
+    to local index within a tensor and qubit type. i.e. physical qubits in tensor 2 start from index 0, 
     and virtual qubits in tensor 2 also start from index 0.
 
     In a contraction, the two tensors are referred to as tensor1 and tensor2, where tensor1
     is the tensor with the lower index in TensorNetwork.tensors
+
+    In a contraction, all shared (virtual qubit) indices between tensor1 and tensor2 will be contracted.
+    The lists of virtual qubit indices corresponding to these contractions are referred to as 
+    tensor1Contractions and tensor2Contractions. The lists of any other virtual qubits which are 
+    not being contracted as they represent entanglements with other tensors, are referred to as
+    tensor1UncontractedVqs and tensor2UncontractedVqs.
 
 **/
 
@@ -76,15 +82,15 @@ int getNumContractions(TensorNetwork tn, int tensor1Index, int tensor2Index);
 
 void getContractionVqIndices(TensorNetwork tn, int tensor1Index, int tensor2Index, int numContractions,
     int **tensor1Contractions, int **tensor2Contractions,
-    int *numTensor1UnusedContractions, int *numTensor2UnusedContractions,
-    int **tensor1UnusedContractions, int **tensor2UnusedContractions);
+    int *numTensor1UncontractedVqs, int *numTensor2UncontractedVqs,
+    int **tensor1UncontractedVqs, int **tensor2UncontractedVqs);
 
 long long int getContractedIndex(long long int activeStateVec1Index, long long int activeStateVec2Index,
      long long int stateVec1PhysicalSize, long long int stateVec2PhysicalSize, int numTensor1Pq, int numTensor2Pq,
-     int numTensor1UnusedContractions, int numTensor2UnusedContractions);
+     int numTensor1UncontractedVqs, int numTensor2UncontractedVqs);
 
 long long int getStateVectorIndexFromActiveIndex(long long int activeStateIndex,
-    int numPq, int *unusedContractions, int numUnusedContractions);
+    int numPq, int *uncontractedVqs, int numUncontractedVqs);
 
 Complex recursiveContract(Tensor tensor1, Tensor tensor2, long long int tensor1Offset,
     long long int tensor2Offset, int *tensor1Contractions, int *tensor2Contractions,
@@ -108,7 +114,7 @@ void removeAllVqVertices(TensorNetwork tn, int tensorIndex);
 
 void removeContractedVqVertices(TensorNetwork tn, int tensorIndex, int newTensorIndex, 
         VqVertex *startingVqVertex, VqVertex *prevVqVertexToKeep,
-        int *unusedContractions, int numUnusedContractions, VqVertex **tail, int *foundHead);
+        int *uncontractedVqs, int numUncontractedVqs, VqVertex **tail, int *foundHead);
 
 
 // ----- INDEXING UTILITY --------------------------------------------------------------
